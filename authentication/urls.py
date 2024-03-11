017cf466d10b160e4e158f4a93bb6fc85b980d06
urooj khan <urooj.khan@lelafe.com>
2024-03-11 16:54:33 +0530

    User will be redirected to dashboard if already authenticated and tries to access login page

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
-    path("login/", auth_views.LoginView.as_view(template_name="authentication/login.html", next_page="/dashboard"), name="login")
+    path("login/", auth_views.LoginView.as_view(template_name="authentication/login.html", next_page="/dashboard", redirect_authenticated_user=True), name="login")
 ]
