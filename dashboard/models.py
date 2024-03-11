c73663d08c565b630d0be5a914ae559ae4aedf40
urooj khan <urooj.khan@lelafe.com>
2024-03-11 22:47:06 +0530

    Started work on BatterCell Model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class BatteryCell(models.Model):
+    # image is stored as a base64 encoded data url
+    image_data_url = models.TextField()
+    cell_id = models.CharField(max_length=10, blank=True)
+    
+    # override the save method so that 10 digit cell_id is generated on
+    # basis of auto-incrementing 'id' AutoField
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.cell_id:
+            self.cell_id = '{:010d}'.format(self.id)
+            super().save(update_fields=['cell_id'])
