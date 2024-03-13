e9029b3fe8ca983c814526f84ad688067407cd0d
urooj khan <urooj.khan@lelafe.com>
2024-03-13 21:11:17 +0530

    Removed livereload server

--- ThinkClockAssessment/settings.py
+++ ThinkClockAssessment/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'authentication',
     'dashboard',
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript'
 ]
 
 ROOT_URLCONF = 'ThinkClockAssessment.urls'
